0eae96bf517f6c6f799ffb5a0826a202e0c80b19
luigim1998 <sluigimuller@gmail.com>
2019-06-11 00:01:13 -0400

    Upload da questao 4

--- QUESTAO_4/TODO_LKM.c
+++ QUESTAO_4/TODO_LKM.c
@@ -1,13 +1,11 @@
 /**
- * @file   ebbchar.c
- * @author Derek Molloy
- * @date   7 April 2015
+ * @file   TODO_LKM.c
+ * @author Luigi Muller
+ * @date   10 June 2019
  * @version 0.1
- * @brief   An introductory character driver to support the second article of my series on
- * Linux loadable kernel module (LKM) development. This module maps to /dev/ebbchar and
- * comes with a helper C program that can be run in Linux user space to communicate with
- * this the LKM.
- * @see http://www.derekmolloy.ie/ for a full description and follow-up descriptions.
+ * @brief   A character driver to support a task list that will run as an LKM. 
+ * This code is based on Derek Molloy's code.
+ * @see http://www.derekmolloy.ie/ for the base code.
  */
 
 #include <linux/init.h>           // Macros used to mark up functions e.g. __init __exit
@@ -16,13 +14,13 @@
 #include <linux/kernel.h>         // Contains types, macros, functions for the kernel
 #include <linux/fs.h>             // Header for the Linux file system support
 #include <linux/uaccess.h>        // Required for the copy to user function
-#include <linux/list.h>           // 
-#define  DEVICE_NAME "ebbchar"    ///< The device will appear at /dev/ebbchar using this value
-#define  CLASS_NAME  "ebb"        ///< The device class -- this is a character device driver
+#include <linux/list.h>           // Required for task list
+#define  DEVICE_NAME "TODO_LIST"    ///< The device will appear at /dev/ebbchar using this value
+#define  CLASS_NAME  "todo"        ///< The device class -- this is a character device driver
 
 MODULE_LICENSE("GPL");            ///< The license type -- this affects available functionality
-MODULE_AUTHOR("Derek Molloy");    ///< The author -- visible when you use modinfo
-MODULE_DESCRIPTION("A simple Linux char driver for the BBB");  ///< The description -- see modinfo
+MODULE_AUTHOR("Luigi Muller");    ///< The author -- visible when you use modinfo
+MODULE_DESCRIPTION("A simple Linux char driver for to do list");  ///< The description -- see modinfo
 MODULE_VERSION("0.1");            ///< A version number to inform users
 
 static int    majorNumber;                  ///< Stores the device number -- determined automatically
@@ -37,6 +35,15 @@ static int     dev_open(struct inode *, struct file *);
 static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+/**
+ * @brief A estrutura conterá a descrição da tarefa a ser feita e uma struct da lista.
+*/
+struct todo_list{
+	char descricao[256];
+	struct list_head list;
+};
+
+struct todo_list todo_head;
 
 /** @brief Devices are represented as file structure in the kernel. The file_operations structure from
  *  /linux/fs.h lists the callback functions that you wish to associated with your file operations
@@ -56,35 +63,38 @@ static struct file_operations fops =
  *  time and that it can be discarded and its memory freed up after that point.
  *  @return returns 0 if successful
  */
-static int __init ebbchar_init(void){
-   printk(KERN_INFO "EBBChar: Initializing the EBBChar LKM\n");
+static int __init todo_init(void){
+   printk(KERN_INFO "Iniciando o TODO list em LKM\n");
 
    // Try to dynamically allocate a major number for the device -- more difficult but worth it
    majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
    if (majorNumber<0){
-      printk(KERN_ALERT "EBBChar failed to register a major number\n");
+      printk(KERN_ALERT "TODO list: falhou em registrar um major number\n");
       return majorNumber;
    }
-   printk(KERN_INFO "EBBChar: registered correctly with major number %d\n", majorNumber);
+   printk(KERN_INFO "TODO list registrado com o major number %d\n", majorNumber);
 
    // Register the device class
    ebbcharClass = class_create(THIS_MODULE, CLASS_NAME);
    if (IS_ERR(ebbcharClass)){                // Check for error and clean up if there is
       unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to register device class\n");
+      printk(KERN_ALERT "Falha em registrar a classe do dispositivo\n");
       return PTR_ERR(ebbcharClass);          // Correct way to return an error on a pointer
    }
-   printk(KERN_INFO "EBBChar: device class registered correctly\n");
+   printk(KERN_INFO "TODO list: classe do dispositivo registrado corretamente\n");
 
    // Register the device driver
    ebbcharDevice = device_create(ebbcharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
    if (IS_ERR(ebbcharDevice)){               // Clean up if there is an error
       class_destroy(ebbcharClass);           // Repeated code but the alternative is goto statements
       unregister_chrdev(majorNumber, DEVICE_NAME);
-      printk(KERN_ALERT "Failed to create the device\n");
+      printk(KERN_ALERT "Falha em criar o dispositivo\n");
       return PTR_ERR(ebbcharDevice);
    }
-   printk(KERN_INFO "EBBChar: device class created correctly\n"); // Made it! device was initialized
+   printk(KERN_INFO "TODO list: dispositivo criado corretamente\n"); // Made it! device was initialized
+
+   INIT_LIST_HEAD(&todo_head.list); //inicia a lista
+
    return 0;
 }
 
@@ -92,12 +102,21 @@ static int __init ebbchar_init(void){
  *  Similar to the initialization function, it is static. The __exit macro notifies that if this
  *  code is used for a built-in driver (not a LKM) that this function is not required.
  */
-static void __exit ebbchar_exit(void){
+static void __exit todo_exit(void){
    device_destroy(ebbcharClass, MKDEV(majorNumber, 0));     // remove the device
    class_unregister(ebbcharClass);                          // unregister the device class
    class_destroy(ebbcharClass);                             // remove the device class
    unregister_chrdev(majorNumber, DEVICE_NAME);             // unregister the major number
-   printk(KERN_INFO "EBBChar: Goodbye from the LKM!\n");
+   struct list_head *pos, *q;
+   struct todo_list * tmp;
+
+   list_for_each_safe(pos, q, &(todo_head.list)){
+       tmp = list_entry(pos, struct todo_list, list);
+       printf("Apagando recado: %s\n", tmp->descricao);
+       list_del(pos);
+       free(tmp);
+    }
+   printk(KERN_INFO "TODO list: Saindo de boas do LKM!\n");
 }
 
 /** @brief The device open function that is called each time the device is opened
@@ -107,7 +126,7 @@ static void __exit ebbchar_exit(void){
  */
 static int dev_open(struct inode *inodep, struct file *filep){
    numberOpens++;
-   printk(KERN_INFO "EBBChar: Device has been opened %d time(s)\n", numberOpens);
+   printk(KERN_INFO "TODO list: Dispositivo foi aberto %d vez(es)\n", numberOpens);
    return 0;
 }
 
@@ -122,14 +141,14 @@ static int dev_open(struct inode *inodep, struct file *filep){
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
    int error_count = 0;
    // copy_to_user has the format ( * to, *from, size) and returns 0 on success
-   error_count = copy_to_user(buffer, message, size_of_message);
+   error_count = copy_to_user(buffer, &(todo_head.descricao), size_of_message);
 
    if (error_count==0){            // if true then have success
-      printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
+      printk(KERN_INFO "TODO list: Enviado %d caracteres para o usuario\n", size_of_message);
       return (size_of_message=0);  // clear the position to the start and return 0
    }
    else {
-      printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
+      printk(KERN_INFO "TODO list: Falha ao enviar %d caracteres para o usuario\n", error_count);
       return -EFAULT;              // Failed -- return a bad address message (i.e. -14)
    }
 }
@@ -143,10 +162,16 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
  *  @param offset The offset if required
  */
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
-   sprintf(message, "%s(%zu letters)", buffer, len);   // appending received string with its length
-   size_of_message = strlen(message);                 // store the length of the stored message
-   printk(KERN_INFO "EBBChar: Received %zu characters from the user\n", len);
-   return len;
+
+    struct todo_list * tmp = (struct todo_list *) kmalloc(sizeof(struct todo_list), GFP_ATOMIC);
+
+    sprintf(tmp->descricao, "%s(%zu letras)", buffer, len);   // appending received string with its length
+    size_of_message = strlen(message);                        // store the length of the stored message
+
+    list_add_tail(&(tmp->list), &todo_head); //Adiciona os nodos
+
+    printk(KERN_INFO "TODO list: Recebido %zu caracteres do usuario\n", len);
+    return len;
 }
 
 /** @brief The device release function that is called whenever the device is closed/released by
@@ -155,7 +180,7 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
  *  @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_release(struct inode *inodep, struct file *filep){
-   printk(KERN_INFO "EBBChar: Device successfully closed\n");
+   printk(KERN_INFO "TODO list: Dispositivo fechado corretamente\n");
    return 0;
 }
 
@@ -163,5 +188,5 @@ static int dev_release(struct inode *inodep, struct file *filep){
  *  identify the initialization function at insertion time and the cleanup function (as
  *  listed above)
  */
-module_init(ebbchar_init);
-module_exit(ebbchar_exit);
+module_init(todo_init);
+module_exit(todo_exit);
